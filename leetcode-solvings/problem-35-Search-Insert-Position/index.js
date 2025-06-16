8aa66f4ca0dbd2568abb65fa7c7d6ca08c6314a7
rabby-shek <rabbyshek1998@gmail.com>
2025-06-17 02:18:49 +0600

    leetcode problem 35 solved

---
+++ leetcode-solvings/problem-35-Search-Insert-Position/index.js
@@ -0,0 +1,27 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var searchInsert = function(nums, target) {
+    let left = 0;
+    let right = nums.length - 1;
+
+    while (left <= right) {
+        const mid = Math.floor((left + right) / 2); 
+        
+        if (nums[mid] === target) {
+            return mid;
+        } else if (nums[mid] < target) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
+    
+    return left; 
+};
+
+const nums = [1,3,5,6];
+let target = 2;
+console.log(searchInsert(nums, target)); // Output: 1
